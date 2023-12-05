63a4abdc6d60d0402276e79e650ddb2b924abd18
etensoep <daanraam@gmail.com>
2023-12-05 19:56:34 +0100

    MovementPart1

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,19 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+{   
+    public float moveSpeed;
+    public Vector2 PlayerInput;
+    public bool isMoving;
 
-    // Update is called once per frame
+    void Start() {
+        moveSpeed = 7f;
+    }
     void Update()
     {
         
+        if(!isMoving) {
+            PlayerInput.x = Input.GetAxisRaw("Horizontal");
+            PlayerInput.y = Input.GetAxisRaw("Vertical");
+            if(PlayerInput != Vector2.zero) 
+            {
+                var targetPos = transform.position;
+                targetPos.x += PlayerInput.x;
+                targetPos.y += PlayerInput.y;
+
+                StartCoroutine(Move(targetPos));
+            }
+        }
     }
+
+    IEnumerator Move(Vector3 targetPos) 
+    {
+        isMoving = true;
+        while((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon) 
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPos;
+        isMoving = false;
+    }
+
 }
