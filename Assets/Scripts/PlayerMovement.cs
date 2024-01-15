e2e0e5ffee641eab3681ea35f97871a6fb5b884a
etensoep <daanraam@gmail.com>
2024-01-15 18:48:25 +0100

    Interactions

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEditor.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {   
@@ -9,12 +10,18 @@ public class PlayerMovement : MonoBehaviour
     public Vector2 PlayerInput;
     public bool isMoving;
     public LayerMask solidObjectsLayer;
+    public LayerMask interactableLayer;
     private Vector3 targetPos = new Vector3(0.5f, 0.5f, 0);
+    private Animator animator;
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
+    }
 
     public Tilemap obstacles;
 
     void Start() {
-        moveSpeed = 7f;
+        moveSpeed = 4f;
     }
     void Update()
     {
@@ -27,14 +34,36 @@ public class PlayerMovement : MonoBehaviour
 
             if(PlayerInput != Vector2.zero) 
             {
+                animator.SetFloat("moveX", PlayerInput.x);
+                animator.SetFloat("moveY", PlayerInput.y);
+
                 targetPos = transform.position + new Vector3(PlayerInput.x, PlayerInput.y, 0);
 		
 		Vector3Int obstacleMap = obstacles.WorldToCell(targetPos);
-		if(obstacles.GetTile(obstacleMap) == null) {		
+		if(IsWalkable(targetPos)) {		
                 StartCoroutine(Move(targetPos));
 		}
             }
         }
+
+    animator.SetBool("isMoving", isMoving);
+
+    if(Input.GetKeyDown(KeyCode.E)) {
+        Interact();
+    }
+
+    void Interact() {
+        var facingDir = new Vector3(animator.GetFloat("moveX"), animator.GetFloat("moveY"),0);
+        var interactPos = transform.position + facingDir;
+        
+        //Debug.DrawLine(transform.position, interactPos, Color.red, 1f);
+
+        var collider = Physics2D.OverlapCircle(interactPos, 0.2f, interactableLayer);
+        if (collider != null) {
+            collider.GetComponent<Interactable>()?.Interact();
+        }
+        }
+
     }
 
     IEnumerator Move(Vector3 targetPos) 
@@ -48,6 +77,14 @@ public class PlayerMovement : MonoBehaviour
         transform.position = targetPos;
         isMoving = false;
     }
+
+    private bool IsWalkable(Vector3 targetPos) {
+        if(Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectsLayer | interactableLayer) != null) {
+            return false;
+        }
+        return true;
+    }
+
 
     void OnDrawGizmos() {
 	Gizmos.color = Color.red;
